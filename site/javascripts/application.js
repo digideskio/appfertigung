e2ce489d94cd5333489ffdd8698ef2762e187559
Eckhard Rotte <erotte@gmail.com>
2011-02-28 00:16:49 +0100

    smooth scrolling script für die Top-Navi-Links - derzeit noch unbenutzt

---
+++ site/javascripts/application.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('a[href*=#]').click(function(event) {
+      $targetElem = $(this.hash);
+      if($targetElem.length){
+        event.preventDefault();
+        $targetTop = $targetElem.offset().top
+        $('html, body').animate({scrollTop: $targetTop}, 1600, 'easeOutBack')
+      }    
+  })
+})    
