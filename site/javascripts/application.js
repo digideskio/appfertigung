ee19732aade39998798f2d9e05e011d501507f6b
Eckhard Rotte <erotte@gmail.com>
2011-02-28 23:02:35 +0100

    history.pushState for scrolling script

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -1,10 +1,18 @@
+// ready for refactoring
+// should also run onhashchange
 $(document).ready(function(){
+  var history_counter = 0;    
   $('a[href*=#]').click(function(event) {
       $targetElem = $(this.hash);
       if($targetElem.length){
         event.preventDefault();
         $targetTop = $targetElem.offset().top
         $('html, body').animate({scrollTop: $targetTop}, 1200, 'easeInOutCubic')
+        if (window.history && window.history.pushState){
+            var new_url = location.href.replace(/\#.+/, this.hash),
+                stateObj = { count : history_counter };
+            window.history.pushState(stateObj, 'page-'+history_counter, new_url) 
+        }
       }    
   })
 })    
