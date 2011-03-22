2a3fec4878313daf59fbadf32ec36037e9660c0d
Eckhard Rotte <erotte@gmail.com>
2011-03-22 15:50:27 +0100

    ScrollFade - A gefixt

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
   // };
   
   var animateAvatar = function() {
-    var scrollY = $('body').scrollTop();
+    var scrollY = $(document).scrollTop();
     var opacity = (scrollY - $('#intro').offset().top) * 0.005;
     if (opacity < 0) opacity = 0;
     else if (opacity > 1) opacity = 1;
