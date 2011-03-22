f7908daf5e50914f7a6f77954871b234b530d845
dbloete <mail@dennisbloete.de>
2011-03-22 13:38:26 +0100

    Random image rotation

--- site/javascripts/application.js
+++ site/javascripts/application.js
@@ -5,7 +5,8 @@ $(document).ready(function(){
     $target = $(hash);
     if ($target.length) {
       event.preventDefault();
-      $('html, body').animate({scrollTop: ($target.offset().top - $('#nav').height())}, 1200, 'easeInOutCubic');
+      var top = ($target.offset().top - $('#nav').height() + 10);
+      $('html, body').animate({scrollTop:top}, 1200, 'easeInOutCubic');
       if (window.history && window.history.pushState) {
         var new_url = /\#/.test(location.href) ? location.href.replace(/\#.+/, hash) : (location.href + hash),
             stateObj = { count : history_counter };
@@ -28,9 +29,16 @@ $(document).ready(function(){
     if (opacity < 0) opacity = 0;
     else if (opacity > 1) opacity = 1;
     $('nav li:first-child').css('opacity', opacity);
-  }
+  };
   
-  $(window).scroll(animateAvatar);
-  animateAvatar();
+  $(window).load(animateAvatar).scroll(animateAvatar);
+  
+  $('.member img').load(function() {
+    var degree = (Math.random()*2-1)*2;
+    $(this).css({ '-webkit-transform': 'rotate(' + degree + 'deg)'});
+    $(this).css({ '-moz-transform': 'rotate(' + degree + 'deg)'});
+    $(this).css({ '-ms-transform': 'rotate(' + degree + 'deg)'});
+    $(this).css({ 'transform': 'rotate(' + degree + 'deg)'});
+  });
   
 });
