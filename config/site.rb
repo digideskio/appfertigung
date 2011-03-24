5a6e640c7e4a096d90ed9c8c00fc575b78ad2103
Stefan Schüßler <stefan@notions.de>
2011-03-24 12:15:48 +0100

    show expanded css in preview mode

--- config/site.rb
+++ config/site.rb
@@ -6,7 +6,7 @@ Compass.configuration do |config|
   # Please update this if it is or remove this message if it can stay the way it is.
   config.project_path = File.dirname(__FILE__)
   config.sass_dir = File.join('src', 'stylesheets')
-  config.output_style = :compressed
+  config.output_style = ARGV[0] == 'build' ? :compressed : :expanded
 end
 
 # sass_engine_options returns a hash, you can merge it with other options.
